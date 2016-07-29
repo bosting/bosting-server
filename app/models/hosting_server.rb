f7d83cb6b341a78b857fd6c6f75077602d3e2fe3
Alexander Zubkov <info@zubkov.info>
2016-07-29 14:40:13 +0300

    Change chef json format

--- app/models/hosting_server.rb
+++ app/models/hosting_server.rb
@@ -25,34 +25,28 @@ class HostingServer < ApplicationRecord
   CHEF_VERSION = '12.12.15'
 
   def to_chef_json
-    hosting_server = serializable_hash
-    hosting_server.keep_if do |key, value|
+    hosting_server_hash = serializable_hash
+    hosting_server_hash.keep_if do |key, value|
       %w(
         fqdn server_domain panel_domain cp_login cp_password ip cores ext_if int_if mysql_version mysql_root_password
         default_mx ns1_domain ns1_ip ns2_domain ns2_ip forward_agent open_tcp_ports open_udp_ports panel_ssl
       ).include?(key)
     end
-    hosting_server['os'] = OSES[os_id].first.downcase
-    hosting_server['mysql_distrib'] = MYSQL_DISTRIBS[mysql_distrib_id].first.downcase
-    hosting_server['delivery_method'] = MAIL_DELIVERY_METHODS[mail_delivery_method_id].first
-    hosting_server['smtp_settings'] = {}
+    hosting_server_hash['mysql_distrib'] = MYSQL_DISTRIBS[mysql_distrib_id].first.downcase
+    hosting_server_hash['delivery_method'] = MAIL_DELIVERY_METHODS[mail_delivery_method_id].first
+    hosting_server_hash['smtp_settings'] = {}
     smtp_settings.each do |ss|
-      hosting_server['smtp_settings'][ss.name] = ss.value
+      hosting_server_hash['smtp_settings'][ss.name] = ss.value
     end
-    hosting_server['apache_variations'] = {}
+    hosting_server_hash['apache_variations'] = {}
     apache_variations.each do |av|
-      hosting_server['apache_variations'][av.name] = {
+      hosting_server_hash['apache_variations'][av.name] = {
           ip: av.ip,
           apache_version: ApacheVariation::APACHE_VERSIONS[av.apache_version_id],
           php_version: ApacheVariation::PHP_VERSIONS[av.php_version_id]
       }
     end
 
-    hosting_server_hash =
-        {
-          'chef_version' => CHEF_VERSION,
-          name => hosting_server
-        }
     hosting_server_hash.to_json
   end
 end

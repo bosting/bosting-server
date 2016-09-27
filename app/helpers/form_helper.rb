3598e1214ca508944af8f0a7fbf87b9eb7bc9a4d
Alexander Zubkov <info@zubkov.info>
2016-09-27 16:03:42 +0300

    Add options_for_horizontal_form

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -7,4 +7,19 @@ module FormHelper
           end
     end
   end
+
+  def options_for_horizontal_form
+    {
+        html: {
+            class: 'form-horizontal'
+        },
+        wrapper: :horizontal_form,
+        wrapper_mappings: {
+            check_boxes: :horizontal_radio_and_checkboxes,
+            radio_buttons: :horizontal_radio_and_checkboxes,
+            file: :horizontal_file_input,
+            boolean: :horizontal_boolean
+        }
+    }
+  end
 end

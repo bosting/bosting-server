c6838a4a1fd8efe320789f34fd53bdb2d7c6b66f
Alexander Zubkov <info@zubkov.info>
2018-10-09 12:06:32 +0300

    Refactor ServerBootstrapper

--- app/services/server_bootstrapper.rb
+++ app/services/server_bootstrapper.rb
@@ -12,20 +12,6 @@ class ServerBootstrapper
 
   def setup
     clone_cookbooks(Rails.root.join('tmp', 'cookbooks').to_s)
-
-    os = @hosting_server.os.capitalize
-    cookbook_path = Rails.root.join('tmp', 'cookbooks', 'bosting-cp').to_s
-    vendor_path = Rails.root.join('tmp', 'vendor', 'cookbooks').to_s
-    klass = Object.const_get("ChefRunners::#{os}")
-    runner = klass.new(
-      credentials:         credentials,
-      chef_install_method: @hosting_server.chef_install_method,
-      chef_version:        HostingServer::CHEF_VERSION,
-      chef_recipes:        %w[bosting-cp::first_run bosting-cp],
-      cookbook_path:       cookbook_path,
-      vendor_path:         vendor_path,
-      chef_json:           @hosting_server.to_chef_json
-    )
     runner.setup
   end
 
@@ -34,6 +20,23 @@ class ServerBootstrapper
   def clone_cookbooks(path)
     FileUtils.rm_rf(path)
     `git clone https://github.com/bosting/bosting-cookbooks.git #{path}`
+  end
+
+  def runner
+    runner_class.new(
+      credentials:         credentials,
+      chef_install_method: @hosting_server.chef_install_method,
+      chef_version:        HostingServer::CHEF_VERSION,
+      chef_recipes:        %w[bosting-cp::first_run bosting-cp],
+      cookbook_path:       Rails.root.join('tmp', 'cookbooks', 'bosting-cp').to_s,
+      vendor_path:         Rails.root.join('tmp', 'vendor', 'cookbooks').to_s,
+      chef_json:           @hosting_server.to_chef_json
+    )
+  end
+
+  def runner_class
+    os = @hosting_server.os.capitalize
+    Object.const_get("ChefRunners::#{os}")
   end
 
   def credentials
